5068f9ee3040e7119ae1e944ef5d506854874e4f
Stephen Chavez <stephen@dicesoft.net>
2018-03-16 16:05:04 -0600

    Fixed route import

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,12 @@
-import { h, Component } from 'preact';
-import { Router } from 'preact-router';
+import {h, Component} from 'preact';
+import {Router} from 'preact-router';
 import Match from 'preact-router/match';
-import { LOGIN_PATH, RESET_PATH, REGISTER_PATH } from "../../config";
+import {LOGIN_PATH, RESET_PATH, REGISTER_PATH} from "../../config";
 
 // import components
 import Nav from './Nav';
 import Logo from './Logo';
-import Pins from './pins';
+import Pins from './Pins';
 
 // import routes
 import Home from '../routes/home';
@@ -23,39 +23,37 @@ import Settings from '../routes/settings';
 const AVAIL_PANE_HEIGHT = screen.availHeight * 0.93;
 
 export default class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			navbarHeight: screen.availHeight - AVAIL_PANE_HEIGHT
-		};
-	}
-	/** Gets fired when the route changes.
+    constructor() {
+        super();
+        this.state = {
+            navbarHeight: screen.availHeight - AVAIL_PANE_HEIGHT
+        };
+    }
+    /** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
 	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
+    handleRoute = e => {
+        this.currentUrl = e.url;
+    };
 
-	render() {
-		return (
-			<div id="app">
-				<Nav navHeight={this.state.navbarHeight}/>
-				<Router onChange={this.handleRoute}>
-					<Home path="/" paneHeight={AVAIL_PANE_HEIGHT}/>
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-					<Account path="/register" paneHeight={AVAIL_PANE_HEIGHT}/>
-					<Account path="/signin" paneHeight={AVAIL_PANE_HEIGHT}/>
-					<Account path="/forgot-password" paneHeight={AVAIL_PANE_HEIGHT}/>
-          				<Account path="/reset-password" paneHeight={AVAIL_PANE_HEIGHT}/>
-          				<Settings path="/settings" paneHeight={AVAIL_PANE_HEIGHT}/>
-					<SignOut path="/signout"/>
-					<Directions path="/directions" />
-					<Pins path="/pins" />
-					<Maps path="/maps" paneHeight={AVAIL_PANE_HEIGHT}/>
-				</Router>
-			</div>
-		);
-	}
+    render() {
+        return (<div id="app">
+            <Nav navHeight={this.state.navbarHeight}/>
+            <Router onChange={this.handleRoute}>
+                <Home path="/" paneHeight={AVAIL_PANE_HEIGHT}/>
+                <Profile path="/profile/" user="me"/>
+                <Profile path="/profile/:user"/>
+                <Account path="/register" paneHeight={AVAIL_PANE_HEIGHT}/>
+                <Account path="/signin" paneHeight={AVAIL_PANE_HEIGHT}/>
+                <Account path="/forgot-password" paneHeight={AVAIL_PANE_HEIGHT}/>
+                <Account path="/reset-password" paneHeight={AVAIL_PANE_HEIGHT}/>
+                <Settings path="/settings" paneHeight={AVAIL_PANE_HEIGHT}/>
+                <SignOut path="/signout"/>
+                <Directions path="/directions"/>
+                <Pins path="/pins"/>
+                <Maps path="/maps" paneHeight={AVAIL_PANE_HEIGHT}/>
+            </Router>
+        </div>);
+    }
 }
